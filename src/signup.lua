d7311c2ccaba10ac27861d22cecff3052680b0a3
zongying.cao <zongying.cao@dxc.com>
2018-12-30 05:33:59 +0000

    refactor the util
    Signed-off-by: zongying.cao <zongying.cao@dxc.com>

--- src/signup.lua
+++ src/signup.lua
@@ -5,90 +5,15 @@ package.path = string.format("%s?.lua;%s?/init.lua;%s?.lua;%s",
 	        p, p, p1, m_package_path)
 
 local mongo = require("resty.mongol");
-local resty_sha1 = require("resty.sha1");
-local json = require("cjson");
-local str = require("resty.string");
+local util = require('util')
 
 ngx.header.content_type = "application/json";
 
 ngx.req.read_body();
 
-local function has_key(rs, key)
-      for k,v in pairs(rs)
-      do
-	if k == key then
-	   return true
-	end
-      end
-
-      return false
-end
-local function get_post_args()
-      local args = ngx.req.get_body_data()
-
-      if args then
-          local rs = json.decode(args)
-
-           return function (name)
-		 if has_key(rs, name) then
-		    return rs[name]
-		 else
-		    return nil
-		 end	 
-
-	   end
-      else
-          return function(name)
-	       return nil
-	  end
-      end
-end
-
-local function get_query_args()
-    local args = ngx.req.get_uri_args()
-
-    if args then
-        return function(name)
-	    if has_key(args, name) then
-	        return args[name]
-	    else
-	        return nil
-            end
-	end
-    else
-        return function(name)
-	    return nil
-	end
-    end
-end
-
-local function get_header_args()
-    local args = ngx.req.get_headers()
-
-    if args then
-        return function(name)
-	    if has_key(args, name) then
-	        return args[name]
-	    else
-	        return nil
-	    end
-	end
-    else
-        return function(name)
-	    return nil
-	end
-    end
-end
-
-local post_args = get_post_args()
-local query_args = get_query_args()
-local header_args = get_header_args()
-
-
 local function signup(name, pwd)
       local conn = mongo:new()
       err, ok = conn:connect(ngx.var.auth_db, ngx.var.auth_db_port)
-      local util = require('util')
 
       
       if err then
@@ -111,5 +36,5 @@ local function signup(name, pwd)
 end
 
 ngx.status = 201
-signup(post_args('user'), post_args('pwd'))
+signup(util.post_args('user'), util.post_args('pwd'))
 ngx.exit(201)
--- src/util.lua
+++ src/util.lua
@@ -1,11 +1,77 @@
 local util = {}
 local resty_sha1 = require("resty.sha1");
 local str = require("resty.string");
+local json = require("cjson");
+
+local function has_key(rs, key)
+      for k,v in pairs(rs)
+      do
+	if k == key then
+	   return true
+	end
+      end
+
+      return false
+end
 
 function util.getsha(data)
       local sha1 = resty_sha1:new()
       sha1:update(data)
       return str.to_hex(sha1:final())
+end
+
+local function get_query_args()
+    local args = ngx.req.get_uri_args()
+
+    if args then
+        return function(name)
+	    if has_key(args, name) then
+	        return args[name]
+	    else
+	        return nil
+            end
+	end
+    else
+        return function(name)
+	    return nil
+	end
+    end
+end
+
+local function get_header_args()
+    local args = ngx.req.get_headers()
+
+    if args then
+        return function(name)
+	    if has_key(args, name) then
+	        return args[name]
+	    else
+	        return nil
+	    end
+	end
+    else
+        return function(name)
+	    return nil
+	end
+    end
+end
+
+function util.post_args(key)
+      local args = ngx.req.get_body_data()
+
+      function get_data()
+          local rs = json.decode(args)
+	  if has_key(rs, key) then
+	     return rs[key]
+	  else
+	     return nil
+	  end	 
+      end
+      if args then
+          return get_data()
+      else
+          return nil
+      end
 end
 
 return util
