10d050d481965de7f1084c244585aa392aa86b7b
zongying.cao <zongying.cao@dxc.com>
2018-12-30 14:48:32 +0000

    improve code
    Signed-off-by: zongying.cao <zongying.cao@dxc.com>

--- src/util.lua
+++ src/util.lua
@@ -38,40 +38,19 @@ local function get_query_args()
     end
 end
 
-local function get_header_args()
-    local args = ngx.req.get_headers()
-
-    if args then
-        return function(name)
-	    if has_key(args, name) then
-	        return args[name]
-	    else
-	        return nil
-	    end
-	end
-    else
-        return function(name)
-	    return nil
-	end
-    end
+function get_arg(args, key)
+     if args and has_key(args, key) then
+	 return args[key]
+     else
+	 return nil
+     end
+end
+function util.header_args(key)
+    return get_arg(ngx.req.get_headers(), key)
 end
 
 function util.post_args(key)
-      local args = ngx.req.get_body_data()
-
-      function get_data()
-          local rs = json.decode(args)
-	  if has_key(rs, key) then
-	     return rs[key]
-	  else
-	     return nil
-	  end	 
-      end
-      if args then
-          return get_data()
-      else
-          return nil
-      end
+      return get_arg(json.decode(ngx.req.get_body_data()), key)
 end
 
 function util.gentoken(seed)
